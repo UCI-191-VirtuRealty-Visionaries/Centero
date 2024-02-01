7c0e9fe502207a108fef7deb6fc592bf7f3d741d
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-01-31 18:01:47 -0800

    Change firebase_config.ts in Backend

--- functions/src/firebase_config.ts
+++ functions/src/firebase_config.ts
@@ -4,13 +4,12 @@
  * the actual firebase instance
  */
 const configApp = {
-  apiKey: "AIzaSyCNJs_VyFxI7OFAaNxOIoL4KEPtSF0mKUc",
-  authDomain: "centerobackend.firebaseapp.com",
-  projectId: "centerobackend",
-  storageBucket: "centerobackend.appspot.com",
-  messagingSenderId: "780428703318",
-  appId: "1:780428703318:web:670d5ac13609f2e9fb6df7",
-  measurementId: "G-VDDP4KG3DZ",
+  apiKey: "AIzaSyBZ5BiOQbVf7PHkhkejEEaOOmf8XNIUY7U",
+  authDomain: "centerobackend-14820.firebaseapp.com",
+  projectId: "centerobackend-14820",
+  storageBucket: "centerobackend-14820.appspot.com",
+  messagingSenderId: "934086295160",
+  appId: "1:934086295160:web:28c5929414a3261752b78d",
 };
 
 export {configApp};
