abc9cc9606e3d810c036a1ebd3c3d005df427d6c
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-02-05 14:48:21 -0800

    Setup Dedicated API Folder for Backend
    - Add `environment` Import Helper
    - Add `/api/helloWorld`
    - Import `/api/clientSignIn`

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,32 +1,8 @@
-// ==================================================
-// Imports
-// ==================================================
-
-// Config
-// import { firebaseCredentials } from "./config/firebase-config";
-// import { cors } from "./config/cors-config";
-
-// Firebase Admin
 import { initializeApp } from "firebase-admin/app";
-// import { getFirestore } from "firebase-admin/firestore";
 
-// Firebase Cloud Functions
-// Full List at https://firebase.google.com/docs/functions
-import { logger } from "firebase-functions";
-import { onRequest } from "firebase-functions/v2/https";
-
-// ==================================================
-// Setup
-// ==================================================
-
+// Configure
 initializeApp();
-// const firestore = getFirestore();
 
-// ==================================================
 // Exports
-// ==================================================
-
-export const helloWorld = onRequest((req, res) => {
-  logger.info("Hello, logs!");
-  res.send("Hello from Centero! (This endpoint is for testing purposes only 🙏)");
-});
+export { helloWorld } from "./api/helloWorld";
+export { clientSignIn } from "./api/clientSignIn";
