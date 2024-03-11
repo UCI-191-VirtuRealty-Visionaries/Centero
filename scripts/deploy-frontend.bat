ffbd94a2e92de663dca724db5abc25056ac58fcf
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-03-10 18:06:23 -0700

    Update Backend to Allow Deploy
    - Simplify deploy-frontend relative paths
    - Remove linting on predeploy
    - Upgrade cloud security to version 2

--- scripts/deploy-frontend.bat
+++ scripts/deploy-frontend.bat
@@ -1,17 +1,17 @@
 @echo off
-pushd "%~dp0"
+pushd "%~dp0/.."
 
 echo ================================
 echo Deploying Front-end as Live Site
 echo ================================
 
 echo ===== Build Flutter for Web =====
-pushd "../frontend/centero"
+pushd "frontend/centero"
 call flutter build web
 popd
 
 echo ===== Deploy to Firebase =====
-pushd "../frontend"
+pushd "frontend"
 call firebase deploy --only hosting
 popd
 
