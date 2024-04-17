5263db3a48409df6340dbe5e2c3f6cbad573a11b
“Michelle <itsmichellelin@gmail.com>
2024-04-17 16:03:33 -0700

    move to age page on click

--- frontend/centero/lib/pages/home.dart
+++ frontend/centero/lib/pages/home.dart
@@ -33,46 +33,55 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/apt_building_bg.jpg', // background image of apartment buildings
-            fit: BoxFit.cover, // cover whole screen
-            width: double.infinity, // image take up whole width
-            height: double.infinity, // image take up whole height
-          ),
-          Center(
-            child: Column (
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 1000), // add padding above the Opacity widget
-                  child: Container(
-                    width: double.infinity, // card take up whole width
-                    height: 2000,
-                    color: Colors.white.withOpacity(0.6),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/centero_logo.png',
-                          width: 500, // logical pixels
-                          height: 500 // logical pixels
-                        ),
-                        Text("CENTERO",
-                          style: TextStyle(fontSize: 300)),
-                        Text("WOULD YOU LIKE TO CHAT?",
-                          style: TextStyle(fontSize: 160)),
-                        SizedBox(height: 90),
-                        Text("Touch screen to start", 
-                          style: TextStyle(fontSize: 85))
-                      ]
-                    )
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AgePage()),
+          );
+        },
+        child:
+        Stack(
+          children: [
+            Image.asset(
+              'assets/apt_building_bg.jpg', // background image of apartment buildings
+              fit: BoxFit.cover, // cover whole screen
+              width: double.infinity, // image take up whole width
+              height: double.infinity, // image take up whole height
+            ),
+            Center(
+              child: Column (
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 1000), // add padding above the Opacity widget
+                    child: Container(
+                      width: double.infinity, // card take up whole width
+                      height: 2000,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/centero_logo.png',
+                            width: 500, // logical pixels
+                            height: 500 // logical pixels
+                          ),
+                          Text("CENTERO",
+                            style: TextStyle(fontSize: 300)),
+                          Text("WOULD YOU LIKE TO CHAT?",
+                            style: TextStyle(fontSize: 160)),
+                          SizedBox(height: 90),
+                          Text("Touch screen to start", 
+                            style: TextStyle(fontSize: 85))
+                        ]
+                      )
+                    ),
                   ),
-                ),
-              ]
-            )
-          ),
-        ]
+                ]
+              )
+            ),
+          ]
+        )
       )
     );
   }
