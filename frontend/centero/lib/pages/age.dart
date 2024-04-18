0bd0321ddb89d0272a3657c7cd02e75c33e695de
Austin-Chun21 <austinchun2021@gmail.com>
2024-04-17 19:36:17 -0700

    Add basic text and buttons
    - Add header text and success/return buttons
    - Move navigate button to end of page

--- frontend/centero/lib/pages/age.dart
+++ frontend/centero/lib/pages/age.dart
@@ -1,23 +1,52 @@
+import 'package:centero/widgets/button_danger.dart';
+import 'package:centero/widgets/button_primary.dart';
 import 'package:flutter/material.dart';
 import 'role_selection.dart';
 
 class AgePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Age Page'), 
+    // ----- Widgets -----
+
+    final headerText = Text(
+      'You must be at least 18 to proceed.',
+      style: TextStyle(
+        fontFamily: 'Montserrat',
       ),
+    );
+
+    final btnProceed = ButtonPrimary(
+      onPressed: () {},
+      child: Text('I am 18+'),
+    );
+
+    final btnReturn = ButtonDanger(
+      onPressed: () {},
+      child: Text('I am NOT 18+'),
+    );
+
+    final navigateToNextPage = ElevatedButton(
+      child: Text('Go to role selection page'),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => RoleSelectionPage()),
+        );
+      },
+    );
+
+    // ----- Structure -----
+
+    return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: Text('Go to role selection page'),
-          onPressed: () {
-            Navigator.push(
-              context, 
-              MaterialPageRoute(builder: (context) => RoleSelectionPage()),
-            );
-          },
-        ),  //Hi Michelle 
+        child: Column(
+          children: [
+            headerText,
+            btnProceed,
+            btnReturn,
+            navigateToNextPage,
+          ],
+        ),
       ),
     );
   }
