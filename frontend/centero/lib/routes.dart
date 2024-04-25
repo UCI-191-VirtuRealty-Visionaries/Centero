fb257bfd1821f07130d5d05307e48c9cb03d5f4a
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-04-24 20:25:38 -0700

    Add Remaining Pages

--- frontend/centero/lib/routes.dart
+++ frontend/centero/lib/routes.dart
@@ -1,8 +1,20 @@
+import 'package:centero/pages/auth_face_scan.dart';
+import 'package:centero/pages/auth_google_pin.dart';
+import 'package:centero/pages/auth_google_register.dart';
+import 'package:centero/pages/call_regular.dart';
+import 'package:centero/pages/call_regular_loading.dart';
+import 'package:centero/pages/call_supervisor.dart';
+import 'package:centero/pages/call_supervisor_loading.dart';
 import 'package:centero/pages/debug_portal.dart';
+import 'package:centero/pages/feedback_bad.dart';
+import 'package:centero/pages/feedback_good.dart';
+import 'package:centero/pages/feedback_select.dart';
 import 'package:centero/pages/info_age.dart';
 import 'package:centero/pages/info_non_resident_role_selection.dart';
 import 'package:centero/pages/info_role_selection.dart';
 import 'package:centero/pages/manager_dashboard.dart';
+import 'package:centero/pages/manager_login.dart';
+import 'package:centero/pages/superuser_login.dart';
 import 'package:centero/pages/welcome_home.dart';
 import 'package:centero/pages/superuser_dashboard.dart';
 import 'package:centero/pages/debug_widget_preview.dart';
@@ -59,6 +71,22 @@ class DevRouteConfig {
       case 'InfoAge': return PageInfoAge();
       case 'InfoRoleSelection': return PageInfoRoleSelection();
       case 'InfoNonResidentRoleSelection': return PageInfoNonResidentRoleSelection();
+      case 'AuthFaceScan': return PageAuthFaceScan();
+      case 'AuthGooglePin': return PageAuthGooglePin();
+      case 'AuthGoogleRegister': return PageAuthGoogleRegister();
+      case 'CallRegularLoading': return PageCallRegularLoading();
+      case 'CallRegular': return PageCallRegular();
+      case 'CallSupervisorLoading': return PageCallSupervisorLoading();
+      case 'CallSupervisor': return PageCallSupervisor();
+      case 'FeedbackSelect': return PageFeedbackSelect();
+      case 'FeedbackGood': return PageFeedbackGood();
+      case 'FeedbackBad': return PageFeedbackBad();
+      case 'ManagerLogin': return PageManagerLogin();
+      case 'ManagerDashboard': return PageManagerDashboard();
+      case 'SuperuserLogin': return PageSuperuserLogin();
+      case 'SuperuserDashboard': return PageSuperuserDashboard();
+      case 'DebugPortal': return PageDebugPortal();
+      case 'DebugWidgetPreview': return PageDebugWidgetPreviewPage();
       default: return PageDebugPortal();
     }
   }
