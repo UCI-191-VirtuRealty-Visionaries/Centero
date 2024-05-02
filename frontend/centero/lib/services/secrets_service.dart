2123838f5056febcb1cede22288990f4a329fd0e
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-05-01 21:21:01 -0700

    Change Secret Strategy to Autogenerating Dart File

--- frontend/centero/lib/services/secrets_service.dart
+++ frontend/centero/lib/services/secrets_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:centero/global_config.dart';
-import 'package:flutter/services.dart';
+import 'package:centero/secrets.dart';
 import 'package:logging/logging.dart';
 
 class SecretsService {
@@ -13,15 +13,18 @@ class SecretsService {
   }
 
   Future<void> _initProdMode() async {
-    try {
-      final inStr =
-          await rootBundle.loadString('assets/secrets/prod_hashkey', cache: false);
-      _hmacKey = utf8.encode(inStr);
-    } catch (err) {
+    const inStr = centeroProdHashKey;
+
+    if (!centeroSecretsFileWasPopulated) {
       _logger.shout(
-          'Could not load prod_hashkey, disabling login to prevent data leakage');
-      _hmacKey = utf8.encode('err_key');
+          'Secrets were not assigned by the deploy script, disabling login to prevent data leakage');
+      _logger.severe(
+          'TEMPORARY: Using dev-mode hashkey to allow app to progress.');
+      _hmacKey = utf8.encode('debug_key');
+      return;
     }
+
+    _hmacKey = utf8.encode(inStr);
   }
 
   Future<void> initialize() async {
