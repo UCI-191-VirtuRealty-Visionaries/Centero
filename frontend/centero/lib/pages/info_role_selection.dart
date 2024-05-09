c291c34c16b0e703b3ec85f1ba827d07e05b164c
“Michelle <itsmichellelin@gmail.com>
2024-05-09 10:07:17 -0700

    place buttons in correct position

--- frontend/centero/lib/pages/info_role_selection.dart
+++ frontend/centero/lib/pages/info_role_selection.dart
@@ -6,69 +6,88 @@ import 'package:centero/styles/app_theme.dart';
 class PageInfoRoleSelection extends StatelessWidget {
   // Work Orders Button
   Widget WorkOrderButton() {
-    return OutlinedButton(onPressed: () {}, 
+    return ElevatedButton(onPressed: () {}, 
       style: ButtonStyle(
         fixedSize: MaterialStateProperty.all<Size>(
-          Size(600.0, 150.0), // Button width and height
+          Size(650.0, 150.0), // Button width and height
         ),
       ),
-      child: Text('Work Orders', style: TextStyle(fontSize: 60)));
+      child: Text('Work Orders', style: TextStyle(fontSize: 90)));
   }
 
   // Amenity Info Button
   Widget AmenityInfoButton () {
-    return OutlinedButton(onPressed: () {}, 
+    return ElevatedButton(onPressed: () {}, 
     style: ButtonStyle(
         fixedSize: MaterialStateProperty.all<Size>(
-          Size(600.0, 150.0), // Button width and height
+          Size(650.0, 150.0), // Button width and height
         ),
       ),
-    child: Text('Amenity Info', style: TextStyle(fontSize: 60)));
+    child: Text('Amenity Info', style: TextStyle(fontSize: 90)));
   }
 
   // Local Events Button
   Widget LocalEventsButton () {
-    return OutlinedButton(onPressed: () {}, 
+    return ElevatedButton(onPressed: () {}, 
     style: ButtonStyle(
         fixedSize: MaterialStateProperty.all<Size>(
-          Size(600.0, 150.0), // Button width and height
+          Size(650.0, 150.0), // Button width and height
         ),
       ),
-    child: Text('Local Events', style: TextStyle(fontSize: 60)));
+    child: Text('Local Events', style: TextStyle(fontSize: 90)));
   }
 
   // Your Lease Button
   Widget YourLeaseButton () {
-    return OutlinedButton(onPressed: () {}, 
+    return ElevatedButton(onPressed: () {}, 
     style: ButtonStyle(
         fixedSize: MaterialStateProperty.all<Size>(
-          Size(600.0, 150.0), // Button width and height
+          Size(650.0, 150.0), // Button width and height
         ),
       ),
-    child: Text('Your Lease', style: TextStyle(fontSize: 60)));
+    child: Text('Your Lease', style: TextStyle(fontSize: 90)));
   }
 
   // Other Button
   Widget OtherButton () {
-    return OutlinedButton(onPressed: () {}, 
+    return ElevatedButton(onPressed: () {}, 
     style: ButtonStyle(
         fixedSize: MaterialStateProperty.all<Size>(
-          Size(600.0, 150.0), // Button width and height
+          Size(650.0, 150.0), // Button width and height
         ),
       ),
-    child: Text('Other', style: TextStyle(fontSize: 60)));
+    child: Text('Other', style: TextStyle(fontSize: 90)));
+  }
+
+  // Exit Button
+  Widget ExitButton () {
+    return Container(
+      width: 1800,
+      child: Row(
+        children: [ElevatedButton(onPressed: () {}, 
+          style: ButtonStyle(
+              fixedSize: MaterialStateProperty.all<Size>(
+                Size(260.0, 100.0), // Button width and height
+              ),
+            ),
+          child: Text('Exit', style: TextStyle(fontSize: 45))),
+        Expanded(child: Container())]
+      )
+    );
   }
 
   // Build column of buttons
   Widget buildButtonColumn() {
-    return Column(children: [WorkOrderButton(), AmenityInfoButton(), LocalEventsButton(), YourLeaseButton(), OtherButton()]);
+    return Column(children: [WorkOrderButton(), SizedBox(height: 60), AmenityInfoButton(), SizedBox(height: 60), LocalEventsButton(), SizedBox(height: 60), YourLeaseButton(), SizedBox(height: 60), OtherButton(), SizedBox(height: 60), ExitButton()]);
   }
 
   @override
   Widget build(BuildContext context) {
     final content = Column(children: [
+      SizedBox(height: 350),
       Text("Hello, Resident!", style: TextStyle(fontSize: 200, color: Colors.white)),
       Text("What would you like to chat about?", style: TextStyle(fontSize: 80, color: Colors.white)),
+      SizedBox(height: 150),
       buildButtonColumn()
     ]);
 
