ba88ca9222287de12014b67ac020374c421590b9
“Michelle <itsmichellelin@gmail.com>
2024-05-15 15:34:56 -0700

    add text and cancel button

--- frontend/centero/lib/pages/call_regular_loading.dart
+++ frontend/centero/lib/pages/call_regular_loading.dart
@@ -1,12 +1,38 @@
 import 'package:centero/widgets/debug_page_placeholder.dart';
+import 'package:centero/widgets/button_secondary.dart';
 import 'package:centero/widgets/page_frame.dart';
 import 'package:flutter/material.dart';
+import 'welcome_home.dart';
 
 class PageCallRegularLoading extends StatelessWidget {
+  // Move to welcome screen
+  void goToWelcomeScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PageWelcomeHome()),
+    );
+  }
+
+  // Cancel Button
+  Widget cancelButton(BuildContext context) {
+    return ButtonSecondary(
+      width: 1500,
+      onPressed: () {goToWelcomeScreen(context);}, 
+      child: Text('Cancel', style: TextStyle(fontSize: 80)));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final content = Column(children: [
+      SizedBox(height: 600),
+      Text("Connecting you now...", 
+        style: TextStyle(fontSize: 140, color: Colors.white, fontFamily: 'Josefin', fontWeight: FontWeight.bold)),
+      cancelButton(context)
+      ]
+    );
+
     return PageFrame(
-      child: DebugPagePlaceholder(message: toString()),
+      child: content
     );
   }
 }
