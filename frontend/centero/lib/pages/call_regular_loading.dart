0ee548490b26c9b8a5805e5419b23f1c27add1be
“Michelle <itsmichellelin@gmail.com>
2024-05-16 09:31:34 -0700

    proceed to next page on tap

--- frontend/centero/lib/pages/call_regular_loading.dart
+++ frontend/centero/lib/pages/call_regular_loading.dart
@@ -4,6 +4,7 @@ import 'package:centero/widgets/page_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'welcome_home.dart';
+import 'call_regular_confirmation.dart';
 
 class PageCallRegularLoading extends StatelessWidget {
   // Move to welcome screen
@@ -30,15 +31,24 @@ class PageCallRegularLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final content = Column(children: [
-      SizedBox(height: 600),
-      Text("Connecting you now...", 
-        style: TextStyle(fontSize: 140, color: Colors.white, fontFamily: 'Josefin', fontWeight: FontWeight.bold)),
-      SizedBox(height: 200),
-      spinkit,
-      SizedBox(height: 200),
-      cancelButton(context)
-      ]
+    final content = GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PageCallRegularConfirmation()),
+        );
+      }, 
+      child: Column(
+        children: [
+          SizedBox(height: 600),
+          Text("Connecting you now...", 
+            style: TextStyle(fontSize: 140, color: Colors.white, fontFamily: 'Josefin', fontWeight: FontWeight.bold)),
+          SizedBox(height: 200),
+          spinkit,
+          SizedBox(height: 200),
+          cancelButton(context)
+        ]
+      )
     );
 
     return PageFrame(
