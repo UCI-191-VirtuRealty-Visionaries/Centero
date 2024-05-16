216a8da0febaf90e290e1489bc2921d836b6e262
“Michelle <itsmichellelin@gmail.com>
2024-05-15 17:20:28 -0700

    add spinning loading circle

--- frontend/centero/lib/pages/call_regular_loading.dart
+++ frontend/centero/lib/pages/call_regular_loading.dart
@@ -2,6 +2,7 @@ import 'package:centero/widgets/debug_page_placeholder.dart';
 import 'package:centero/widgets/button_secondary.dart';
 import 'package:centero/widgets/page_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'welcome_home.dart';
 
 class PageCallRegularLoading extends StatelessWidget {
@@ -21,12 +22,21 @@ class PageCallRegularLoading extends StatelessWidget {
       child: Text('Cancel', style: TextStyle(fontSize: 80)));
   }
 
+  // Loading Circle
+  final spinkit = const SpinKitFadingCircle(
+            color: Colors.green,
+            size: 500
+          );
+
   @override
   Widget build(BuildContext context) {
     final content = Column(children: [
       SizedBox(height: 600),
       Text("Connecting you now...", 
         style: TextStyle(fontSize: 140, color: Colors.white, fontFamily: 'Josefin', fontWeight: FontWeight.bold)),
+      SizedBox(height: 200),
+      spinkit,
+      SizedBox(height: 200),
       cancelButton(context)
       ]
     );
