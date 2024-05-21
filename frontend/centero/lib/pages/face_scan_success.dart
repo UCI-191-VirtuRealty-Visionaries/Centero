6d579fdb9af8cdf24a488906e9852b39e3c852cf
Austin-Chun21 <austinchun2021@gmail.com>
2024-05-21 11:53:53 -0700

    Created success and fail face scan pages

---
+++ frontend/centero/lib/pages/face_scan_success.dart
@@ -0,0 +1,109 @@
+import 'package:centero/pages/face_scan_fail.dart';
+import 'package:centero/widgets/button_secondary.dart';
+import 'package:centero/widgets/page_frame.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:centero/widgets/button_danger.dart';
+import 'package:centero/widgets/button_primary.dart';
+import 'face_scan.dart';
+import 'info_age.dart';
+import 'resident_call_options.dart';
+import 'welcome_home.dart';
+
+class FaceScanSuccess extends StatefulWidget {
+  //Stack
+  @override
+  State<FaceScanSuccess> createState() => _FaceScanSuccessState();
+}
+  
+class _FaceScanSuccessState extends State<FaceScanSuccess> {
+  void showPopup(text) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(text)),
+    );
+  }
+
+Widget faceScanner() {
+      return SvgPicture.asset(
+        'assets/face_scan_reticle.svg',
+        width: 1000,
+        height: 1000,
+      );
+    }
+
+    Widget headerText() {
+      return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text("Scan Successful!",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Josefin',
+              fontWeight: FontWeight.normal,
+              fontSize: 80,
+              color: Color.fromARGB(255, 59, 189, 15),
+            )),
+      ]);
+    }
+
+    Widget scanSuccess() {
+      return Image.asset(
+        'assets/scansuccess.png',
+        width: 500, // logical pixels
+        height: 500, // logical pixels
+      );
+    }
+
+    Widget bottomText() {
+      return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text("We will connect you now!",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Josefin',
+              fontWeight: FontWeight.normal,
+              fontSize: 60,
+              color: Colors.white,
+            )),
+      ]);
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    //Widgets
+    final content = GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FaceScanFail()),
+        );
+      },
+      child: Stack(
+        alignment: Alignment(0, -0.6),
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(
+            top: 80,
+          )),
+          //btnProceed(),
+          Positioned(top: 650, child: faceScanner()),
+          Column(
+            children: <Widget>[
+              SizedBox(height: 800),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 750, right: 750), // Add margin around the Text widget
+                child: headerText(),
+              ),
+              SizedBox(height: 50),
+              scanSuccess(),
+              SizedBox(height: 250),
+              bottomText()
+            ],
+          ),
+        ],
+      ),
+    );
+    return PageFrame(
+      child: content,
+      );
+  }
+}
--- frontend/centero/lib/widgets/button_secondary.dart
+++ frontend/centero/lib/widgets/button_secondary.dart
@@ -14,7 +14,7 @@ class ButtonSecondary extends ElevatedButton {
       foregroundColor: Colors.white,
       backgroundColor: Colors.grey.shade300.withOpacity(0.25),
       side: BorderSide(width: 5.0, color: Colors.white),
-      fixedSize: const Size(850, 200)
+      fixedSize: const Size(750, 190)
     );
   }
 }
