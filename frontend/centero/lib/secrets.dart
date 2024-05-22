6d1001c2707879cbd4138d819c405ec333eddb04
Bryan <10124208+anguillifax@users.noreply.github.com>
2024-05-21 21:17:03 -0700

    Configure Firebase API Key Injection

--- frontend/centero/lib/secrets.dart
+++ frontend/centero/lib/secrets.dart
@@ -1,10 +1,4 @@
 // The GitHub action replaces the contents of this file on deployment.
 const centeroSecretsFileWasPopulated = false;
 const centeroProdHashKey = '';
-const centeroFirebaseApiKey = '';
-const centeroFirebaseAppId = '';
-const centeroFirebaseMessagingSenderId = '';
-const centeroFirebaseProjectId = '';
-const centeroFirebaseAuthDomain = '';
-const centeroFirebaseDatabaseURL = '';
-const centeroFirebaseStorageBucket = '';
+const centeroFirebaseApiKey = 'AIzaSyBZ5BiOQbVf7PHkhkejEEaOOmf8XNIUY7U'; // Local debug key
