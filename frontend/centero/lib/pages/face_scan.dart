dc64d51c434e9ae8efdbf4a75f51b3ae78052536
“Michelle <itsmichellelin@gmail.com>
2024-05-22 18:23:15 -0700

    add rectangles

--- frontend/centero/lib/pages/face_scan.dart
+++ frontend/centero/lib/pages/face_scan.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:centero/widgets/page_frame.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'resident_call_options.dart';
+import 'welcome_home.dart';
 
 class FaceScan extends StatelessWidget {
   Widget headerText() {
@@ -25,15 +26,42 @@ class FaceScan extends StatelessWidget {
       );
   }
 
-  
+  Widget residentBackground() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: 1400,
+          height: 1700,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 3.0)),
+          child: SvgPicture.asset(
+            'assets/confident_woman.svg',
+            fit: BoxFit.cover)
+        ), 
+        Positioned(
+          top: 100,
+          child: faceScanner())
+      ]
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
-    final content = Column(children: [
-      SizedBox(height: 650),
-      headerText(),
-      faceScanner()
-    ]);
+    final content = GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PageWelcomeHome()),
+        );
+      },
+      child: Column(children: [
+        SizedBox(height: 650),
+        headerText(),
+        SizedBox(height: 100),
+        residentBackground()
+      ])
+    );
 
     return PageFrame(
       child: content,
