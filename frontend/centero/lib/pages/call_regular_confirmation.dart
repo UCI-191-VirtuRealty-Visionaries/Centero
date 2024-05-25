756cb8e5daaad41bbe05bf0a28d91467c87362e2
cjmangune <cmangune@uci.edu>
2024-05-25 14:15:21 -0700

    Create call loading confirmation page
    - Added call_confirmed.png to assets
    - Updated call_regular_loading.dart to navigate to call_regular_confirmation.dart

--- frontend/centero/lib/pages/call_regular_confirmation.dart
+++ frontend/centero/lib/pages/call_regular_confirmation.dart
@@ -1,12 +1,52 @@
-import 'package:centero/widgets/debug_page_placeholder.dart';
 import 'package:centero/widgets/page_frame.dart';
 import 'package:flutter/material.dart';
+// import 'call_regular.dart';
+import 'feedback_bad.dart'; // delete later
 
 class PageCallRegularConfirmation extends StatelessWidget {
+  // Move to call regular screen
+  void nextScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PageFeedbackBad()),
+      // update to PageCallRegular() when complete
+    );
+  }
+
+  // Green checkmark PNG
+  Widget callConnected () { 
+    return Image.asset(
+      'assets/call_confirmed.png',
+      width: 500,
+      height: 500,
+    );
+  }
+
   @override
+  // Build column
   Widget build(BuildContext context) {
+    final content = GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PageFeedbackBad()),
+        );
+      },
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 600),
+          Text("Ready to Chat!",
+            style: TextStyle(fontSize: 140, color: Colors.white, fontFamily: 'Josefin', fontWeight: FontWeight.bold)),
+          Text("Connecting you now to Mark Smith...",
+            style: TextStyle(fontSize: 80, color: Colors.white, fontFamily: 'Josefin', fontWeight: FontWeight.bold)),
+          SizedBox(height: 200),
+          callConnected(),
+        ],
+      ),
+    );
+
     return PageFrame(
-      child: DebugPagePlaceholder(message: toString()),
+      child: content
     );
   }
 }
